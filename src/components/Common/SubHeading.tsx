d129f57e41a00057a2665e92d7b58b592d9a4d1c
Asad234322 <asad_ismail@ragzon.com>
2024-02-22 12:45:28 +0500

    fixed conflict

--- src/components/Common/SubHeading.tsx
+++ src/components/Common/SubHeading.tsx
@@ -9,7 +9,7 @@ const SubHeading = ({
 }: propsTypes) => {
   return (
     <p
-      className={`text-[40px] font-bold pb-6 text-lightBlackColor ${className}`}>
+      className={`text-[22px] font-bold pb-6 text-lightBlackColor ${className}`}>
       {heading}
     </p>
   );
