67a2e5138f3bd60437554f89ae5a5cfa2b3d1e8b
Asad234322 <asad_ismail@ragzon.com>
2024-02-22 12:24:14 +0500

    add hover border in bar

--- src/components/Courses/CourseDetail/CourseDetailBanner.tsx
+++ src/components/Courses/CourseDetail/CourseDetailBanner.tsx
@@ -17,34 +17,34 @@ const CourseDetailBanner = () => {
     <div className="flex">
       <div className="w-8/12 mb-10">
         <DetailBanner />
-        <div className="flex gap-4 text-mainParaColor border-b-2 border-b-mainParaColor pb-1.5 mb-8 mr-5 ml-24 mt-10">
+        <div className="flex gap-4 text-mainParaColor border-b-2 border-b-mainParaColor mb-8 mr-5 ml-24 mt-10">
           <span
-            className={`font-medium text-base hover:text-btnColor cursor-pointer ${
-              currentStep === 1 && "text-btnColor"
+            className={` font-medium text-base hover:text-btnColor hover:border-btnColor hover:border-b-2 mb-[-2px] cursor-pointer ${
+              currentStep === 1 && "text-btnColor border-btnColor border-b-2"
             }`}
             onClick={() => handleStepClick(1)}
           >
             Overview
           </span>
           <span
-            className={`font-medium text-base hover:text-btnColor cursor-pointer ${
-              currentStep === 2 && "text-btnColor"
+            className={` font-medium text-base hover:text-btnColor hover:border-btnColor hover:border-b-2 mb-[-2px] cursor-pointer ${
+              currentStep === 2 && "text-btnColor border-btnColor border-b-2"
             }`}
             onClick={() => handleStepClick(2)}
           >
             Course Content
           </span>
           <span
-            className={`font-medium text-base hover:text-btnColor cursor-pointer ${
-              currentStep === 3 && "text-btnColor"
+            className={`font-medium text-base hover:text-btnColor hover:border-btnColor hover:border-b-2 mb-[-2px] cursor-pointer ${
+              currentStep === 3 && "text-btnColor border-btnColor border-b-2"
             }`}
             onClick={() => handleStepClick(3)}
           >
             Instructors
           </span>
           <span
-            className={`font-medium text-base hover:text-btnColor cursor-pointer ${
-              currentStep === 4 && "text-btnColor"
+            className={`font-medium text-base hover:text-btnColor hover:border-btnColor hover:border-b-2 mb-[-2px] cursor-pointer ${
+              currentStep === 4 && "text-btnColor border-btnColor border-b-2"
             }`}
             onClick={() => handleStepClick(4)}
           >
