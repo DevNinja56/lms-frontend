10fa6f16820be912064b4658328f52ac4d560ff8
Asad234322 <asad_ismail@ragzon.com>
2024-02-22 13:13:28 +0500

    fixed PR

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,18 +2,14 @@ import React, { useState } from "react";
 import whiteLogo from "../../../public/images/logo.svg";
 import { ROUTES } from "@route/constants.route";
 import { NavLink, useNavigate } from "react-router-dom";
-<<<<<<< HEAD
-import { loggedIn } from "@slices/auth.slice";
-
-const Navbar = () => {
-  const user = loggedIn();
-=======
 import TabNavbar from "@components/TabNavbar";
 import SVG from "./SVG";
 import SVGPortalIcon from "./SVGPortalIcon";
+import { loggedIn } from "@slices/auth.slice";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const user = loggedIn();
 
   const toggleMenu = () => {
     setIsOpen(!isOpen);
@@ -22,7 +18,6 @@ const Navbar = () => {
     setIsOpen(false);
   };
 
->>>>>>> e99f234a5fe182407236e317e7f1a51e9a3a075c
   const isHomePage = window.location.pathname === ROUTES.USERHOME;
   const navigate = useNavigate();
   const navigateToHome = () => {
@@ -33,21 +28,13 @@ const Navbar = () => {
     <div
       className={`h-24 w-full flex justify-between items-center relative ${
         isHomePage ? "bg-white" : "bg-mainColor text-white"
-<<<<<<< HEAD
-      } px-24`}
-=======
       } md:px-24 sm:px-8 xs:px-4`}
->>>>>>> e99f234a5fe182407236e317e7f1a51e9a3a075c
     >
       <div
         onClick={navigateToHome}
         className={`flex items-center justify-between py-4 px-2 sticky top-0 ${
           isHomePage ? "bg-mainColor" : "bg-[#6b87db] "
-<<<<<<< HEAD
-        } rounded-full `}
-=======
         } rounded-full`}
->>>>>>> e99f234a5fe182407236e317e7f1a51e9a3a075c
       >
         <img className="" src={whiteLogo} />
       </div>
@@ -111,11 +98,7 @@ const Navbar = () => {
         </NavLink>
         <NavLink to={ROUTES.POPULAR_COURSES}>
           <span
-<<<<<<< HEAD
-            className={`text-sm inline-block font-medium  py-2.5 px-5 w-full ${
-=======
             className={`text-sm lg:inline-block md:block font-medium  py-2.5 px-5 w-full ${
->>>>>>> e99f234a5fe182407236e317e7f1a51e9a3a075c
               window.location.pathname === ROUTES.POPULAR_COURSES
                 ? "border-b"
                 : ""
@@ -126,11 +109,7 @@ const Navbar = () => {
         </NavLink>
         <NavLink to={ROUTES.BUY_COURSES}>
           <span
-<<<<<<< HEAD
-            className={`text-sm inline-block font-medium  py-2.5 px-5 w-full ${
-=======
             className={`text-sm lg:inline-block md:block font-medium  py-2.5 px-5 w-full ${
->>>>>>> e99f234a5fe182407236e317e7f1a51e9a3a075c
               window.location.pathname === ROUTES.BUY_COURSES ? "border-b" : ""
             }`}
           >
@@ -139,59 +118,20 @@ const Navbar = () => {
         </NavLink>
         <NavLink to={ROUTES.FAQ}>
           <span
-<<<<<<< HEAD
-            className={`text-sm inline-block font-medium  py-2.5 px-5 w-full ${
-              window.location.pathname === ROUTES.FAQ ? "border-b" : ""
-            }`}
-=======
             className={`text-sm lg:inline-block md:block font-medium  py-2.5 px-5 w-full`}
->>>>>>> e99f234a5fe182407236e317e7f1a51e9a3a075c
           >
             FAQs
           </span>
         </NavLink>
 
         <NavLink to={ROUTES.CHECKOUT}>
-<<<<<<< HEAD
-          <span className="text-sm inline-block font-medium  py-2.5 px-5 w-full ">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-            >
-              <path
-                d="M3.74181 18.5545C4.94143 20 7.17414 20 11.6395 20H12.3607C16.8261 20 19.0589 20 20.2585 18.5545M3.74181 18.5545C2.54219 17.1091 2.95365 14.9146 3.77657 10.5257C4.36179 7.40452 4.65441 5.84393 5.7653 4.92196M20.2585 18.5545C21.4581 17.1091 21.0466 14.9146 20.2237 10.5257C19.6385 7.40452 19.3459 5.84393 18.235 4.92196M18.235 4.92196C17.1241 4 15.5363 4 12.3607 4H11.6395C8.46398 4 6.8762 4 5.7653 4.92196"
-                stroke="#495057"
-                strokeWidth="2"
-                style={{
-                  stroke: isHomePage ? "#495057" : "white",
-                }}
-              />
-              <path
-                d="M9.1709 8C9.58273 9.16519 10.694 10 12.0002 10C13.3064 10 14.4177 9.16519 14.8295 8"
-                stroke="#495057"
-                strokeWidth="2"
-                strokeLinecap="round"
-                style={{
-                  stroke: isHomePage ? "#495057" : "white",
-                }}
-              />
-            </svg>
-=======
           <span className="text-sm lg:inline-block md:block font-medium  py-2.5 px-5 w-full ">
             <SVGPortalIcon />
->>>>>>> e99f234a5fe182407236e317e7f1a51e9a3a075c
           </span>
         </NavLink>
         <NavLink to={user ? ROUTES.HOMEPAGE : ROUTES.SIGN_IN}>
           <span
-<<<<<<< HEAD
-            className={`text-sm inline-block font-medium py-2.5 rounded-md px-5 w-full  ${
-=======
             className={`text-sm lg:inline-block font-medium py-2.5 rounded-md px-5 w-full  ${
->>>>>>> e99f234a5fe182407236e317e7f1a51e9a3a075c
               isHomePage ? "bg-mainColor text-white" : "bg-white text-mainColor"
             }`}
           >
